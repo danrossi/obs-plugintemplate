b83ff55c63d58d01499797076bc2ee0b03b57a3b
Dan Rossi <electroteque@protonmail.com>
2026-05-05 00:06:07 +1000

    - fix Windows install path

--- cmake/windows/defaults.cmake
+++ cmake/windows/defaults.cmake
@@ -8,9 +8,11 @@ set(CMAKE_FIND_PACKAGE_TARGETS_GLOBAL TRUE)
 include(buildspec)
 
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  set(ALLUSERSPROFILE "$ENV{ALLUSERSPROFILE}")
+  cmake_path(SET ALLUSERSPROFILE_PATH $ENV{ALLUSERSPROFILE})
   set(
     CMAKE_INSTALL_PREFIX
-    "$ENV{ALLUSERSPROFILE}/obs-studio/plugins"
+    "${ALLUSERSPROFILE_PATH}/obs-studio/plugins"
     CACHE STRING
     "Default plugin installation directory"
     FORCE
